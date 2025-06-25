9522194b17504bf4ebdb0c3c7ff18fc34bc2f268
Nimesh04 <nimeshb907@gmail.com>
2025-06-25 14:41:09 -0500

    Solved #6 problem

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let repeatedWord = '';
+    if(num < 0){
+        repeatedWord = "ERROR";
+    } 
+    else{
+        for(let i = 0; i < num; i++){
+        repeatedWord += word;
+    }
+    }
+    
+    return repeatedWord;
 };
 
 // Do not edit below this line
