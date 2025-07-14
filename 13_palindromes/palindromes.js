7dc9eba508acf6b831e6b5e4c68d659345a928d2
Nimesh04 <nimeshb907@gmail.com>
2025-07-14 14:38:31 -0500

    Completed exercise 13 of js chapter

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let alphaNumber = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let checkReady = word.toLowerCase()
+                        .split('')
+                        .filter((char) => alphaNumber.includes(char))
+                        .join('');
+    let reverseWord = checkReady.split('').reverse().join('');
+    return checkReady === reverseWord;
 };
 
 // Do not edit below this line
