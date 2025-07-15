a8690c5db8727cf4456f82e75eaa7373bd2a8f52
Nimesh04 <nimeshb907@gmail.com>
2025-07-15 10:13:45 -0500

    Completed exercises 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if(pos < 0 ) return "OOPS";
+    if(pos == 0) return 0;
+    let prev = 0;
+    let curr = 1;
+    let arr = [];
+    let sum;
+    for(let i = 0; i < pos; i++){
+        if(i==0){
+            arr.push(curr);
+        }else{
+            sum = prev + curr;
+            arr.push(sum);
+            prev = arr[arr.length - 2];
+            curr = sum;
+        }
+    }
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
