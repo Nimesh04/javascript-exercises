4f702db92378f66b716209694c325874a64ff58d
Nimesh04 <nimeshb907@gmail.com>
2025-07-15 11:51:10 -0500

    Completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    array.forEach(element => {
+        if(element.yearOfDeath == undefined){
+            let date = new Date().getFullYear();
+            element.yearOfDeath =  date;
+        }
+    });
+        
 
+    
+    let oldest = array.map((person) => 
+            ({
+            name: person.name,
+            age: person.yearOfDeath - person.yearOfBirth
+        })
+    );
+    let oldest1 = oldest.sort((a,b) =>b.age - a.age);
+    console.log(oldest);
+    return oldest1[0];
 };
 
 // Do not edit below this line
